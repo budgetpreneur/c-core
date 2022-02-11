e94ab43f401a7c34cc3fe825ff70186e903823d6
Pandu Masabathula <budgetpreneur@users.noreply.github.com>
2022-02-11 17:19:03 +0530

    Update pubnub_sync_sample.c

--- core/samples/pubnub_sync_sample.c
+++ core/samples/pubnub_sync_sample.c
@@ -3,6 +3,7 @@
 
 #include "core/pubnub_helper.h"
 #include "core/pubnub_timers.h"
+#include "core/pubnub_proxy.h"
 
 #include <stdio.h>
 #include <time.h>
@@ -80,6 +81,7 @@ int main()
         return -1;
     }
     pubnub_init(pbp, "demo", "demo");
+    pubnub_set_proxy_manual(pbp, pbproxyHTTP_CONNECT, "127.0.0.1", 26379);
 
     pubnub_set_transaction_timeout(pbp, PUBNUB_DEFAULT_SUBSCRIBE_TIMEOUT);
 
